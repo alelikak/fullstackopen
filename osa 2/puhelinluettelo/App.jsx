c3d8bcde5f59eefb17896a896218c2cb29e18171
alelikak <141032811+alelikak@users.noreply.github.com>
2023-08-30 09:35:06 -0400

    Create App.jsx Exercise 2.6

---
+++ osa 2/puhelinluettelo/App.jsx
@@ -0,0 +1,49 @@
+import { useState } from 'react'
+
+
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+  const [newNote, setNewNote] = useState(    'a new note...'  ) 
+  const [showAll, setShowAll] = useState(true)
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    
+    const personObject = {
+      name: newNote,
+      important: Math.random() > 0.5,
+      id: persons.length + 1,
+    }
+  
+    setPersons(persons.concat(personObject))
+    setNewNote('')
+  }
+
+  const personsToShow = showAll    ? persons    : persons.filter(person => person.important === true)
+  const handleNoteChange = (event) => {    console.log(event.target.value)    
+    setNewNote(event.target.value)  }
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newNote} onChange={handleNoteChange}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <div>debug: {newName}</div>
+      <h2>Numbers</h2>
+      <div>{personsToShow.map(person => <li key={person.name}>  <p>{person.name}</p> </li>)} </div>
+    </div>
+    
+  )
+
+}
+
+export default App
