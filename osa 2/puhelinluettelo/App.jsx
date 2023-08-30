ff14f941620f400e1a247f4ad1e1ca6ac0ef94a8
alelikak <141032811+alelikak@users.noreply.github.com>
2023-08-30 10:50:06 -0400

    Update App.jsx 2.8

--- osa 2/puhelinluettelo/App.jsx
+++ osa 2/puhelinluettelo/App.jsx
@@ -4,10 +4,11 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas' ,number:'050 332 3000' }
   ]) 
   const [newName, setNewName] = useState('')
   const [newNote, setNewNote] = useState(    'a new note...'  ) 
+  const [newNumber, setNewNumber] = useState(    ''  ) 
   const [showAll, setShowAll] = useState(true)
 
   const addPerson = (event) => {
@@ -15,6 +16,7 @@ const App = () => {
     
     const personObject = {
       name: newNote,
+      number:newNumber,
       important: Math.random() > 0.5,
       id: persons.length + 1,
     }
@@ -30,20 +32,24 @@ const App = () => {
   const personsToShow = showAll    ? persons    : persons.filter(person => person.important === true)
   const handleNoteChange = (event) => {    console.log(event.target.value)    
     setNewNote(event.target.value)  }
+    const handleNumberChange = (event) => {    console.log(event.target.value)    
+      setNewNumber(event.target.value)  }
   return (
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newNote} onChange={handleNoteChange}/>
+          
         </div>
+        <div>number: <input value={newNumber} onChange={handleNumberChange}/></div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <div>debug: {newName}</div>
       <h2>Numbers</h2>
-      <div>{personsToShow.map(person => <li key={person.name}>  <p>{person.name}</p> </li>)} </div>
+      <div>{personsToShow.map(person => <li key={person.name}>  <p>{person.name}{" "} {person.number}</p> </li>)} </div>
     </div>
     
   )
