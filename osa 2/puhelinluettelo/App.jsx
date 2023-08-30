b4eb7a04dd59fb1822438a50f05a780827f99209
alelikak <141032811+alelikak@users.noreply.github.com>
2023-08-30 11:09:15 -0400

    Update App.jsx 2.9

--- osa 2/puhelinluettelo/App.jsx
+++ osa 2/puhelinluettelo/App.jsx
@@ -4,8 +4,13 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' ,number:'050 332 3000' }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ])
+
+  const [newFilter, setNewFilter] = useState('')
   const [newName, setNewName] = useState('')
   const [newNote, setNewNote] = useState(    'a new note...'  ) 
   const [newNumber, setNewNumber] = useState(    ''  ) 
@@ -30,14 +35,22 @@ const App = () => {
 
   
   const personsToShow = showAll    ? persons    : persons.filter(person => person.important === true)
+  const personsToShow2 = newFilter    ? persons.filter(person => person.name.toLowerCase().includes(newFilter)):persons
+   //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/includes
+   //https://stackoverflow.com/questions/48979908/react-filter-function-case-insensitive
+  
   const handleNoteChange = (event) => {    console.log(event.target.value)    
     setNewNote(event.target.value)  }
-    const handleNumberChange = (event) => {    console.log(event.target.value)    
-      setNewNumber(event.target.value)  }
+  const handleNumberChange = (event) => {    console.log(event.target.value)    
+    setNewNumber(event.target.value)  }
+  const handleFilterChange = (event) => {    console.log(event.target.value)    
+    setNewFilter(event.target.value)  }
   return (
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={addPerson}>
+      <div>filter shown with: <input value={newFilter} onChange={handleFilterChange}/></div>
+       
         <div>
           name: <input value={newNote} onChange={handleNoteChange}/>
           
@@ -49,7 +62,7 @@ const App = () => {
       </form>
       <div>debug: {newName}</div>
       <h2>Numbers</h2>
-      <div>{personsToShow.map(person => <li key={person.name}>  <p>{person.name}{" "} {person.number}</p> </li>)} </div>
+      <div>{personsToShow2.map(person => <li key={person.name}>  <p>{person.name}{" "} {person.number}</p> </li>)} </div>
     </div>
     
   )
